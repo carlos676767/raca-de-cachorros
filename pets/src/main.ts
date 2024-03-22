9a4fcca6468d8eedd64191a714ceacf478417e97
carlos676767 <carlitobr730@gmail.com>
2024-03-22 09:07:23 -0700

    funcoes organiadas

--- pets/src/main.ts
+++ pets/src/main.ts
@@ -11,29 +11,36 @@ const gerarImagensAletaorias = () => {
     .then((data) => {
       console.log(data);
       data.forEach((elemento: any) => {
-        const {url} = elemento
-        criarImagem(url)
+        const {url} = elemento;
+        criarImagem(url);
       });
     });
 };
 
-const botaoLike = document.querySelector(".fa-heart") as HTMLButtonElement
-
-const addEremoveClasses = (classe1: string, classe2: string) => {
-  botaoLike.classList.add(classe1)
-  botaoLike.classList.remove(classe2)
+const botaoLike = document.querySelector(".fa-heart") as HTMLElement
+const addEremoveClasses = (botao: HTMLElement,classe1: string, classe2: string) => {
+  botao.classList.add(classe1);
+  botao.classList.remove(classe2);
 }
+
 const botaoDaCorCurtir = () => {
-  addEremoveClasses("iDaCorRed", "iDaCorBlack");
+  addEremoveClasses(botaoLike,"iDaCorRed", "iDaCorBlack");
   setTimeout(() => {
-    addEremoveClasses("iDaCorBlack","iDaCorRed");
+    addEremoveClasses(botaoLike,"iDaCorBlack","iDaCorRed");
   }, 1000);
 }
 
-gerarImagensAletaorias()
-
+gerarImagensAletaorias();
 botaoLike.addEventListener("click", () => {
-  gerarImagensAletaorias()
-  botaoDaCorCurtir()
+  gerarImagensAletaorias();
+  botaoDaCorCurtir();
+})
+
+const botaoGostei = document.querySelector(".fa-thumbs-up") as HTMLElement
+botaoGostei.addEventListener("click", () => {
+  addEremoveClasses(botaoGostei, "likeAul", "likeBlack")
+  setTimeout(() => {
+    addEremoveClasses(botaoGostei,"likeBlack", "likeAul")
+  }, 1000);
 })
 
